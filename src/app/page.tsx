55dfc6e402577bd1e5427af17c3119ebc6eef594
Deivid Marlon <deividmarlon@gmail.com>
2024-03-21 08:31:20 -0300

    force text-blakc in app/page.tsx | set title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,19 @@ export default function Home() {
         <Image src={logo.src} alt="logo unioeste" height={180} width={350} />
 
         <div>
-          <h1 className="text-center text-2xl font-bold"> Organização e Arquitetura de Computadores</h1>
-          <p className="text-center text-xl">Simulador de Arquitetura RiscV Monociclo</p>
+          <h1 className="text-center text-black text-2xl font-bold"> Organização e Arquitetura de Computadores</h1>
+          <p className="text-center text-black text-xl">Simulador de Arquitetura RiscV Monociclo</p>
         </div>
 
         <div>
-          <h2 className="text-center text-xl font-bold">Professora:</h2>
-          <p className="text-center">Camile Frazao Bordini</p>
+          <h2 className="text-center text-black text-xl font-bold">Professora:</h2>
+          <p className="text-center text-black">Camile Frazao Bordini</p>
         </div>
 
         <div>
-          <h2 className="text-center text-xl font-bold">Alunos:</h2>
-          <p className="text-center">Daniel Zonta Ojeda</p>
-          <p className="text-center">Deivid Márlon Fernandes da Costa</p>
+          <h2 className="text-center text-black text-xl font-bold">Alunos:</h2>
+          <p className="text-center text-black">Daniel Zonta Ojeda</p>
+          <p className="text-center text-black">Deivid Márlon Fernandes da Costa</p>
 
         </div>
 
